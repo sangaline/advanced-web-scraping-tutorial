178e0a127adda1238f46297a0c79238408ef1718
Evan Sangaline <esangaline@gmail.com>
2017-03-15 15:18:52 -0700

    Add the bypass_threat_defense method.

--- zipru_scraper/middlewares.py
+++ zipru_scraper/middlewares.py
@@ -29,3 +29,31 @@ class ThreatDefenceRedirectMiddleware(RedirectMiddleware):
 
     def is_threat_defense_url(self, url):
         return '://zipru.to/threat_defence.php' in url
+
+    def bypass_threat_defense(self, url=None):
+        # only navigate if any explicit url is provided
+        if url:
+            self.dryscrape_session.visit(url)
+
+        # solve the captcha if there is one
+        captcha_images = self.dryscrape_session.css('img[src *= captcha]')
+        if len(captcha_images) > 0:
+            return self.solve_captcha(captcha_images[0])
+
+        # click on any explicit retry links
+        retry_links = self.dryscrape_session.css('a[href *= threat_defence]')
+        if len(retry_links) > 0:
+            return self.bypass_threat_defense(retry_links[0].get_attr('href'))
+
+        # otherwise, we're on a redirect page so wait for the redirect and try again
+        self.wait_for_redirect()
+        return self.bypass_threat_defense()
+
+    def wait_for_redirect(self, url = None, wait = 0.1, timeout=10):
+        url = url or self.dryscrape_session.url()
+        for i in range(int(timeout//wait)):
+            time.sleep(wait)
+            if self.dryscrape_session.url() != url:
+                return self.dryscrape_session.url()
+        logger.error(f'Maybe {self.dryscrape_session.url()} isn\'t a redirect URL?')
+        raise Exception('Timed out on the zipru redirect page.')
