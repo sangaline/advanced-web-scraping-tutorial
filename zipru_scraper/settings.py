569d3c311982e6ccbaf7e3141e99beb69cbaf3ef
Evan Sangaline <esangaline@gmail.com>
2017-03-15 15:14:42 -0700

    Set the user agent.

--- zipru_scraper/settings.py
+++ zipru_scraper/settings.py
@@ -15,8 +15,7 @@ SPIDER_MODULES = ['zipru_scraper.spiders']
 NEWSPIDER_MODULE = 'zipru_scraper.spiders'
 
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'zipru_scraper (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
