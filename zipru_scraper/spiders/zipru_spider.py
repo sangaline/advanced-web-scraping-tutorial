f5f44b1ea5a28454ebac790d27c49dac319e7f62
Evan Sangaline <esangaline@gmail.com>
2017-03-15 15:14:04 -0700

    Add torrent item extraction to the spider.

--- zipru_scraper/spiders/zipru_spider.py
+++ zipru_scraper/spiders/zipru_spider.py
@@ -9,3 +9,17 @@ class ZipruSpider(scrapy.Spider):
         for page_url in response.css('a[title ~= page]::attr(href)').extract():
             page_url = response.urljoin(page_url)
             yield scrapy.Request(url=page_url, callback=self.parse)
+
+        # extract the torrent items
+        for tr in response.css('table.lista2t tr.lista2'):
+            tds = tr.css('td')
+            link = tds[1].css('a')[0]
+            yield {
+                'title' : link.css('::attr(title)').extract_first(),
+                'url' : response.urljoin(link.css('::attr(href)').extract_first()),
+                'date' : tds[2].css('::text').extract_first(),
+                'size' : tds[3].css('::text').extract_first(),
+                'seeders': int(tds[4].css('::text').extract_first()),
+                'leechers': int(tds[5].css('::text').extract_first()),
+                'uploader': tds[7].css('::text').extract_first(),
+            }
