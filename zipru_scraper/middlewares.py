87ba85f88fa3011263eec8ff0e414a29ad73091e
Evan Sangaline <esangaline@gmail.com>
2017-03-15 15:16:14 -0700

    Add the incomplete threat defence middleware.

--- zipru_scraper/middlewares.py
+++ zipru_scraper/middlewares.py
@@ -1,56 +1,22 @@
-# -*- coding: utf-8 -*-
+import os, tempfile, time, sys, logging
+logger = logging.getLogger(__name__)
 
-# Define here the models for your spider middleware
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/spider-middleware.html
+import dryscrape
+import pytesseract
+from PIL import Image
 
-from scrapy import signals
+from scrapy.downloadermiddlewares.redirect import RedirectMiddleware
 
+class ThreatDefenceRedirectMiddleware(RedirectMiddleware):
+    def _redirect(self, redirected, request, spider, reason):
+        # act normally if this isn't a threat defense redirect
+        if not self.is_threat_defense_url(redirected.url):
+            return super()._redirect(redirected, request, spider, reason)
 
-class ZipruScraperSpiderMiddleware(object):
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the spider middleware does not modify the
-    # passed objects.
+        logger.debug(f'Zipru threat defense triggered for {request.url}')
+        request.cookies = self.bypass_threat_defense(redirected.url)
+        request.dont_filter = True # prevents the original link being marked a dupe
+        return request
 
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
-
-    def process_spider_input(response, spider):
-        # Called for each response that goes through the spider
-        # middleware and into the spider.
-
-        # Should return None or raise an exception.
-        return None
-
-    def process_spider_output(response, result, spider):
-        # Called with the results returned from the Spider, after
-        # it has processed the response.
-
-        # Must return an iterable of Request, dict or Item objects.
-        for i in result:
-            yield i
-
-    def process_spider_exception(response, exception, spider):
-        # Called when a spider or process_spider_input() method
-        # (from other spider middleware) raises an exception.
-
-        # Should return either None or an iterable of Response, dict
-        # or Item objects.
-        pass
-
-    def process_start_requests(start_requests, spider):
-        # Called with the start requests of the spider, and works
-        # similarly to the process_spider_output() method, except
-        # that it doesn’t have a response associated.
-
-        # Must return only requests (not items).
-        for r in start_requests:
-            yield r
-
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+    def is_threat_defense_url(self, url):
+        return '://zipru.to/threat_defence.php' in url
